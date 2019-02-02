7422aef26f83563a52f08cca768ce5ab65db63b5
rais <raismuf@gmail.com>
2019-02-03 06:33:59 +0700

    bilangan terbesar dari 3 buah yang diinputkan

---
+++ latih2.py
@@ -0,0 +1,15 @@
+a=int (input ('bilangan : '))
+b=int (input ('bilangan : '))
+c=int (input ('bilangan : '))
+
+if a>b :
+	if a>c :
+		print(a)
+	else :
+		print (c)
+elif b>c :
+	print(b)
+else :
+	print (c)
+
+
